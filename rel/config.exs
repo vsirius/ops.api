980803e35a28aff2bad90f13f031f3b9a2acfbc5
Pavel Vesnin <paul@nebo15.com>
2017-05-29 18:30:09 +0300

    release configs

--- rel/config.exs
+++ rel/config.exs
@@ -2,16 +2,14 @@ use Mix.Releases.Config,
   default_release: :default,
   default_environment: :default
 
-cookie = :sha256
-|> :crypto.hash(System.get_env("ERLANG_COOKIE") || "ZS3RsvmAmHmNsJiZbd8ZjAWngo8zzZF0IGYOvUsb58Wc6vuFhtrGubsp7XQVV3Oo")
-|> Base.encode64
-
 environment :default do
   set pre_start_hook: "bin/hooks/pre-start.sh"
   set dev_mode: false
   set include_erts: false
   set include_src: false
-  set cookie: cookie
+  set overlays: [
+    {:template, "rel/templates/vm.args.eex", "releases/<%= release_version %>/vm.args"}
+  ]
 end
 
 release :ops do
