d511d65cc500ad1dfecc625435de97464dea8670
MBatrak <maxim.batrak@finstar.com>
2017-07-07 16:36:46 +0300

    Changed migration to make sure the data is not lost

--- priv/repo/migrations/20170703221035_change_declarations.exs
+++ priv/repo/migrations/20170703221035_change_declarations.exs
@@ -4,12 +4,10 @@ defmodule OPS.Repo.Migrations.ChangeDeclarations do
   def change do
     alter table(:declarations) do
       remove :declaration_signed_id
-      remove :employee_id
-      add :employee_id, :uuid, null: false
-      remove :person_id
-      add :person_id, :uuid, null: false
-      remove :legal_entity_id
-      add :legal_entity_id, :uuid, null: false
     end
+
+    execute "alter table declarations alter column employee_id type uuid using employee_id::uuid;"
+    execute "alter table declarations alter column person_id type uuid using person_id::uuid;"
+    execute "alter table declarations alter column legal_entity_id type uuid using legal_entity_id::uuid;"
   end
 end
