a7f13f9e013c548be2dc4b456ee36030ada63962
gmile <iamexile@gmail.com>
2017-07-19 17:22:09 +0300

    Add comment + fix dogma issue

--- priv/repo/migrations/20170719140502_add_declaration_request_id_to_decl.exs
+++ priv/repo/migrations/20170719140502_add_declaration_request_id_to_decl.exs
@@ -6,7 +6,10 @@ defmodule OPS.Repo.Migrations.AddDeclarationRequestIdToDecl do
       add :declaration_request_id, :uuid, null: false
     end
 
-    execute "create extension \"uuid-ossp\";"
+    # This will update existing declarations with UUIDs referring to non-existing declaration_requests.
+    # The code at this point is not in production, so it's safe to do this (another option would be dropping
+    # existing data)
+    execute ~s(create extension "uuid-ossp";)
     execute "update declarations set declaration_request_id = uuid_generate_v4() where declaration_request_id is null;"
   end
 end
