73ad038b9b1606695cce7c7812563d269fc9a190
gmile <iamexile@gmail.com>
2017-07-20 12:41:41 +0300

    Fix migration
    This migration did not apply yet. Safe to fix in place.

--- priv/repo/migrations/20170719140502_add_declaration_request_id_to_decl.exs
+++ priv/repo/migrations/20170719140502_add_declaration_request_id_to_decl.exs
@@ -3,7 +3,7 @@ defmodule OPS.Repo.Migrations.AddDeclarationRequestIdToDecl do
 
   def change do
     alter table(:declarations) do
-      add :declaration_request_id, :uuid, null: false
+      add :declaration_request_id, :uuid
     end
 
     # This will update existing declarations with UUIDs referring to non-existing declaration_requests.
@@ -11,5 +11,9 @@ defmodule OPS.Repo.Migrations.AddDeclarationRequestIdToDecl do
     # existing data)
     execute ~s(create extension "uuid-ossp";)
     execute "update declarations set declaration_request_id = uuid_generate_v4() where declaration_request_id is null;"
+
+    alter table(:declarations) do
+      modify :declaration_request_id, :uuid, null: false
+    end
   end
 end
