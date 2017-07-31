f9ea6a256c2a36936466a9f8898c2d2615b322d9
gmile <iamexile@gmail.com>
2017-07-31 11:08:50 +0300

    Update existing declarations with default seed
    See this commit https://github.com/edenlabllc/ehealth.api/commit/5159c2a1e18ab20dc30297e612a4353b1c98f2cb
    and this PR https://github.com/edenlabllc/ehealth.api/pull/668

--- priv/repo/migrations/20170728132548_add_seed_to_declarations.exs
+++ priv/repo/migrations/20170728132548_add_seed_to_declarations.exs
@@ -3,7 +3,13 @@ defmodule OPS.Repo.Migrations.AddSeedToDeclarations do
 
   def change do
     alter table(:declarations) do
-      add :seed, :string, null: false
+      add :seed, :string
+    end
+
+    execute("update declarations set seed = '99bc78ba577a95a11f1a344d4d2ae55f2f857b98';")
+
+    alter table(:declarations) do
+      modify :seed, :string, null: false
     end
   end
 end
