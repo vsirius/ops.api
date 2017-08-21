5101d42bdaf13631c8bdad1a02a181ba1bf2cbf1
gmile <iamexile@gmail.com>
2017-08-21 12:30:04 +0300

    Update test script

--- test.rb
+++ test.rb
@@ -6,13 +6,15 @@ template = JSON.parse(template_file)
 
 conn = PG.connect(dbname: 'ops_dev')
 
-DAYS = 7
-PER_DAY = 10..20
+DAYS = 14
+PER_DAY = 100..200
 
 DAYS.times do |_day|
   seed = conn.exec("SELECT hash FROM seeds ORDER BY inserted_at DESC LIMIT 1").map { |row| row["hash"] }[0]
 
-  PER_DAY.to_a.sample.times do |_declaration|
+  samples = PER_DAY.to_a.sample
+
+  samples.times do |_declaration|
     # TODO: drop this table and instead add a column to declarations table
     conn.exec("
       INSERT INTO signed_declarations (
@@ -67,6 +69,33 @@ DAYS.times do |_day|
     conn.exec("INSERT INTO signed_declarations (id, signed_data) VALUES (uuid_generate_v4(), '#{template}')")
   end
 
-  # TODO: new_hash should be calculated in PG
-  conn.exec("INSERT INTO seeds VALUES (uuid_generate_v4(), '[#{1}]', now());")
+  conn.exec("
+    INSERT INTO seeds VALUES (
+      uuid_generate_v4(),
+      (SELECT
+        DIGEST(
+          ARRAY_AGG(
+            CONCAT(
+              id,
+              employee_id,
+              start_date,
+              end_date,
+              signed_at,
+              created_by,
+              is_active,
+              scope,
+              division_id,
+              legal_entity_id,
+              inserted_at,
+              declaration_request_id
+            )
+          )::text,
+          'sha512'
+        ) FROM declarations),
+        now()
+      )
+    "
+  )
+
+  puts "Day #{_day + 1}: generated #{samples} declarations."
 end
