ea784f7a20f954b29aa360a5e0cb499a3cb2efb6
gmile <iamexile@gmail.com>
2017-08-21 11:33:51 +0300

    Moving on with the script

--- test.rb
+++ test.rb
@@ -6,51 +6,67 @@ template = JSON.parse(template_file)
 
 conn = PG.connect(dbname: 'ops_dev')
 
-2.times do |i|
+DAYS = 7
+PER_DAY = 10..20
+
+DAYS.times do |_day|
   seed = conn.exec("SELECT hash FROM seeds ORDER BY inserted_at DESC LIMIT 1").map { |row| row["hash"] }[0]
 
-  conn.exec("
-    INSERT INTO declarations (
-      id,
-      employee_id,
-      person_id,
-      start_date,
-      end_date,
-      status,
-      signed_at,
-      created_by,
-      updated_by,
-      is_active,
-      scope,
-      division_id,
-      legal_entity_id,
-      inserted_at,
-      updated_at,
-      declaration_request_id,
-      seed
-    ) VALUES (
-      uuid_generate_v4(),
-      '#{template["employee"]["id"]}',
-      '#{template["person"]["id"]}',
-      '#{template["start_date"]}',
-      '#{template["end_date"]}',
-      '#{template["status"]}',
-      now(),
-      'CCC6C85B-C4DC-43FC-8E75-BA9B855EA597',
-      'FB7FF889-4D20-4F00-BAF5-B9E2D3618341',
-      'true',
-      '#{template["scope"]}',
-      '#{template["division"]["id"]}',
-      '#{template["legal_entity"]["id"]}',
-      now(),
-      now(),
-      '3BA18EA0-09A7-4D5D-9330-029E02DD29AB',
-      '#{seed}'
-    )"
-  )
+  PER_DAY.to_a.sample.times do |_declaration|
+    # TODO: drop this table and instead add a column to declarations table
+    conn.exec("
+      INSERT INTO signed_declarations (
+        id,
+        signed_data
+      ) VALUES (
+        uuid_generate_v4(),
+        '#{template_file}'
+      );
+    ")
 
-  conn.exec("INSERT INTO signed_declarations (id, signed_data) VALUES (uuid_generate_v4(), '#{template}')")
+    conn.exec("
+      INSERT INTO declarations (
+        id,
+        employee_id,
+        person_id,
+        start_date,
+        end_date,
+        status,
+        signed_at,
+        created_by,
+        updated_by,
+        is_active,
+        scope,
+        division_id,
+        legal_entity_id,
+        inserted_at,
+        updated_at,
+        declaration_request_id,
+        seed
+      ) VALUES (
+        uuid_generate_v4(),
+        '#{template["employee"]["id"]}',
+        '#{template["person"]["id"]}',
+        '#{template["start_date"]}',
+        '#{template["end_date"]}',
+        '#{template["status"]}',
+        now(),
+        'CCC6C85B-C4DC-43FC-8E75-BA9B855EA597',
+        'FB7FF889-4D20-4F00-BAF5-B9E2D3618341',
+        'true',
+        '#{template["scope"]}',
+        '#{template["division"]["id"]}',
+        '#{template["legal_entity"]["id"]}',
+        now(),
+        now(),
+        '3BA18EA0-09A7-4D5D-9330-029E02DD29AB',
+        '#{seed}'
+      )"
+    )
+
+    conn.exec("INSERT INTO signed_declarations (id, signed_data) VALUES (uuid_generate_v4(), '#{template}')")
+  end
 
   # TODO: new_hash should be calculated in PG
-  conn.exec("INSERT INTO seeds VALUES (uuid_generate_v4(), '#{new_hash}', now());")
+  conn.exec("INSERT INTO seeds VALUES (uuid_generate_v4(), '[#{1}]', now());")
 end
