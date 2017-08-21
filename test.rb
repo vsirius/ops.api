5c9e582be15cf9725d4e80321efbbe3a14e22bb2
gmile <iamexile@gmail.com>
2017-08-21 13:19:51 +0300

    Move on with test script

--- test.rb
+++ test.rb
@@ -9,13 +9,24 @@ conn = PG.connect(dbname: 'ops_dev')
 DAYS = 14
 PER_DAY = 100..200
 
-DAYS.times do |_day|
-  seed = conn.exec("SELECT hash FROM seeds ORDER BY inserted_at DESC LIMIT 1").map { |row| row["hash"] }[0]
+conn.exec("
+  DELETE FROM seeds;
+  DELETE FROM declarations;
+  DELETE FROM signed_declarations;
 
+  INSERT INTO seeds (id, hash, inserted_at) VALUES (uuid_generate_v4(), digest('Слава Україні!', 'sha512'), '2014-01-01 23:59:59');
+")
+
+DAYS.times do |day|
+  yesterday = (Date.new(2014, 1, 1) + day).to_s
+  today = (Date.new(2014, 1, 1) + day + 1).to_s
+
+  seed = conn.exec("SELECT hash FROM seeds ORDER BY inserted_at DESC LIMIT 1").map { |row| row["hash"] }[0]
   samples = PER_DAY.to_a.sample
 
   samples.times do |_declaration|
     # TODO: drop this table and instead add a column to declarations table
+    # TODO: include seed value in template_file json
     conn.exec("
       INSERT INTO signed_declarations (
         id,
@@ -59,8 +70,8 @@ DAYS.times do |_day|
         '#{template["scope"]}',
         '#{template["division"]["id"]}',
         '#{template["legal_entity"]["id"]}',
-        now(),
-        now(),
+        '#{today}',
+        '#{today}',
         '3BA18EA0-09A7-4D5D-9330-029E02DD29AB',
         '#{seed}'
       )"
@@ -69,11 +80,12 @@ DAYS.times do |_day|
     conn.exec("INSERT INTO signed_declarations (id, signed_data) VALUES (uuid_generate_v4(), '#{template}')")
   end
 
+  # TODO: include new column here
   conn.exec("
     INSERT INTO seeds VALUES (
       uuid_generate_v4(),
       (SELECT
-        DIGEST(
+        digest(
           ARRAY_AGG(
             CONCAT(
               id,
@@ -87,15 +99,18 @@ DAYS.times do |_day|
               division_id,
               legal_entity_id,
               inserted_at,
-              declaration_request_id
+              declaration_request_id,
+              seed
             )
           )::text,
           'sha512'
-        ) FROM declarations),
-        now()
-      )
-    "
-  )
+        ) FROM declarations WHERE DATE(inserted_at) = '#{today}'
+      ),
+      '#{today} 23:59:59'
+    )
+  ")
 
-  puts "Day #{_day + 1}: generated #{samples} declarations."
+  puts "Day #{today}: generated #{samples} declarations."
 end
+
+# Verify
