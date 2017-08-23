310887effc85c83f73e870ed51a2cabd86099fe3
vsirius <siriusgti@gmail.com>
2017-08-24 01:43:36 +0300

    chenged travis.yml

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,6 +4,6 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 ls $TRAVIS_BUILD_DIR/priv
 ls $TRAVIS_BUILD_DIR
-gcloud auth  activate-service-account  --key-file= "${GOOGLE_APPLICATION_CREDENTIALS}"
+gcloud auth  activate-service-account  --key-file=$TRAVIS_BUILD_DIR/eh-test-44e7d0ea33e2.json
 gcloud container clusters get-credentials cluster-1 --zone europe-west1-b --project eh-test-176611
 kubectl get pod --all-namespaces=true
