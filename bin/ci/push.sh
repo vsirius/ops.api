7597c599b31df57970dda10e61f206fd91705be3
vsirius <siriusgti@gmail.com>
2017-08-25 10:15:22 +0300

    chenged travis.yaml

--- bin/ci/push.sh
+++ bin/ci/push.sh
@@ -22,6 +22,8 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   if [ "$TRAVIS_BRANCH" == "$RELEASE_BRANCH" ]; then
     ./bin/ci/release.sh -a $DOCKER_HUB_ACCOUNT -t $TRAVIS_BRANCH -l;
+    ./bin/deploy.sh;
+    ./bin/wait-for-deployment api ops;
   fi;
 
   if [[ "$MAIN_BRANCHES" =~ "$TRAVIS_BRANCH" ]]; then
