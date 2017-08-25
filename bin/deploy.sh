c56ebf7f9a73cae5f74d89fbd9b5fc99f85e955f
vsirius <siriusgti@gmail.com>
2017-08-25 13:18:13 +0300

    chenged travis.yaml

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -28,3 +28,4 @@ ls $PROJECT_NAME
 sed -i'' -e "1,10s/tag:.*/tag: \"$PROJECT_VERSION\"/g" "$Chart/values.yaml"
 #sed -i'' -e "1,10s/repository:.*/repository: \"$PROJECT_REP\"/g" "ops/values.yaml"
 cat $Chart/values.yaml
+helm upgrade  -f $Chart/values.yaml  $Chart $Chart
