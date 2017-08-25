c92ace3f220c5c02144508c324e5cf6079076516
vsirius <siriusgti@gmail.com>
2017-08-25 14:14:27 +0300

    chenged travis.yaml

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -11,8 +11,6 @@ mv linux-amd64/helm /usr/local/bin/helm
 # Credentials to GCE
 gcloud auth  activate-service-account  --key-file=$TRAVIS_BUILD_DIR/eh-test-44e7d0ea33e2.json
 gcloud container clusters get-credentials cluster-1 --zone europe-west1-b --project eh-test-176611
-kubectl get pod --all-namespaces=true
-helm ls
 #get helm charts
 git clone https://$GITHUB_TOKEN@github.com/edenlabllc/ehealth.charts.git
 cd ehealth.charts
@@ -21,16 +19,11 @@ git checkout charts_for_tests
 PROJECT_NAME=$(sed -n 's/.*app: :\([^, ]*\).*/\1/pg' "$TRAVIS_BUILD_DIR/mix.exs")
 PROJECT_VERSION=$(sed -n 's/.*@version "\([^"]*\)".*/\1/pg' "$TRAVIS_BUILD_DIR/mix.exs")
 #PROJECT_VERSION="0.1.261"
-echo "$PROJECT_NAME  $PROJECT_VERSION"
-pwd
-ls $PROJECT_NAME
-#sed -i '' "1,10s/tag:.*/tag: \"33\"/" "ops/values.yaml"
 sed -i'' -e "1,10s/tag:.*/tag: \"$PROJECT_VERSION\"/g" "$Chart/values.yaml"
-#sed -i'' -e "1,10s/repository:.*/repository: \"$PROJECT_REP\"/g" "ops/values.yaml"
-cat $Chart/values.yaml
 helm upgrade  -f $Chart/values.yaml  $Chart $Chart
 cd $TRAVIS_BUILD_DIR/bin
 ./wait-for-deployment.sh api $Chart 
+echo "$?"
    if [$? -eq 0 ]; then
      kubectl get pod -n$Chart | grep api
      cd $TRAVIS_BUILD_DIR/ehealth.charts && git add . && sudo  git commit -m "Bump $Chart version $PROJECT_VERSION" && sudo git pull && sudo git push
