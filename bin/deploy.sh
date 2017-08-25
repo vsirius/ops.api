59af2cd3efa1b00991c00c98ebb2ea28ef8473d9
vsirius <siriusgti@gmail.com>
2017-08-25 14:21:15 +0300

    chenged travis.yaml

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -20,14 +20,14 @@ PROJECT_NAME=$(sed -n 's/.*app: :\([^, ]*\).*/\1/pg' "$TRAVIS_BUILD_DIR/mix.exs"
 PROJECT_VERSION=$(sed -n 's/.*@version "\([^"]*\)".*/\1/pg' "$TRAVIS_BUILD_DIR/mix.exs")
 #PROJECT_VERSION="0.1.261"
 sed -i'' -e "1,10s/tag:.*/tag: \"$PROJECT_VERSION\"/g" "$Chart/values.yaml"
-helm upgrade  -f $Chart/values.yaml  $Chart $Chart
+helm upgrade  -f $Chart/values.yaml  $Chart $Chart 
 cd $TRAVIS_BUILD_DIR/bin
-./wait-for-deployment.sh api $Chart &&
-   if [$? -eq 0 ]; then
-     kubectl get pod -n$Chart | grep api &&
+./wait-for-deployment.sh api $Chart 
+   if ["$?" -eq 0 ]; then
+     kubectl get pod -n$Chart | grep api 
      cd $TRAVIS_BUILD_DIR/ehealth.charts && git add . && sudo  git commit -m "Bump $Chart version $PROJECT_VERSION" && sudo git pull && sudo git push
    else 
-   	 kubectl logs $(sudo kubectl get pod -n$Chart | awk '{ print $1 }' | grep api) -n$Chart &&
+   	 kubectl logs $(sudo kubectl get pod -n$Chart | awk '{ print $1 }' | grep api) -n$Chart 
    	 helm rollback $Chart  $(($(helm ls | grep $Chart | awk '{ print $2 }') -1))
    fi;
 fi;
